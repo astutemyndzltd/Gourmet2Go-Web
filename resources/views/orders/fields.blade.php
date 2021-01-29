966fac9c1bddb79aa3f5ef2072c496741962dc34
Anik Banerjee <anik@astutemyndz.com>
2021-01-30 00:41:52 +0530

    Timer Integration v2.0

--- resources/views/orders/fields.blade.php
+++ resources/views/orders/fields.blade.php
@@ -56,7 +56,7 @@
         foreach($range as $value) $duration->$value = $value . ' minutes';
     ?>
 
-    <input type="hidden" value="{{ json_encode($order->statusDetails) }}">
+    <input type="hidden" value="{{ $lastsFor }}">
 
     <div class="form-group row status-duration-container" style="display:{{ $display }};">
         {!! Form::label('status_duration', 'Estimated Time', ['class' => 'col-3 control-label text-right']) !!}
