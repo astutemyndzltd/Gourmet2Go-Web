4d8a912262ef28e927087ac05e2665f2b14e41a9
Anik Banerjee <anik@astutemyndz.com>
2021-05-14 19:24:08 +0530

    bug fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -141,10 +141,12 @@ class User extends Authenticatable implements HasMedia
 
     public function getCustomFieldsAttribute()
     {
-        $hasCustomField = in_array(static::class,setting('custom_field_models',[]));
-        if (!$hasCustomField){
+        $hasCustomField = in_array(static::class, setting('custom_field_models',[]));
+
+        if (!$hasCustomField) {
             return [];
         }
+
         $array = $this->customFieldsValues()
             ->join('custom_fields', 'custom_fields.id', '=', 'custom_field_values.custom_field_id')
 //            ->where('custom_fields.in_table', '=', true)
